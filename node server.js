bbab3e4ba1c5817e8183a73c713169c786274a04
ThithipongRuesupa <Rockqr.rocker@gmail.com>
2024-03-07 03:59:46 +0700

    pm-test

--- node server.js
+++ node server.js
@@ -14,7 +14,7 @@ app.get("/books/:id", (req, res) => {
 });
 app.post("/books", (req, res) => {
   books.push(req.body);
-  res.status(201).json(req.body);
+  res.status(200).json(req.body);
 });
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -28,7 +28,7 @@ app.put('/books/:id', (req, res) => {
   app.delete('/books/:id', (req, res) => {
     const deletedIndex = books.findIndex(book => book.id === req.params.id)
     books.splice(deletedIndex, 1)
-    res.status(204).send()
+    res.status(200).send()
  })
 
 app.listen(3000, () => {
