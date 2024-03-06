7b6fdaf28b965f45bbe1e446c3f8938f1d81c62b
ThithipongRuesupa <Rockqr.rocker@gmail.com>
2024-03-07 03:46:29 +0700

    books-id

--- node server.js
+++ node server.js
@@ -6,11 +6,12 @@ const books = require('./db')
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-
-
 app.get('/books', (req, res) => {
   res.json(books)
 })
+app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+  })
 
 
 app.listen(3000, () => {
