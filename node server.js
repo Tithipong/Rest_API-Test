48d8b161a3d3ad4b764af5bd4dfdd261b2d148ed
ThithipongRuesupa <Rockqr.rocker@gmail.com>
2024-03-07 03:44:38 +0700

    books

--- node server.js
+++ node server.js
@@ -1,10 +1,17 @@
 // server.js
 const express = require('express')
 const app = express()
+const books = require('./db')
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+
+app.get('/books', (req, res) => {
+  res.json(books)
+})
+
 
 app.listen(3000, () => {
   console.log('http://localhost:3000')
